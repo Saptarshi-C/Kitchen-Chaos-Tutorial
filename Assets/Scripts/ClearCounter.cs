f65b98aae327aecaced016b151b519a76359eb15
Saptarshi Chakraborty <saptarshi.office@gmail.com>
2023-09-18 18:41:25 +0530

    Kitchen Object Parent
    * Let the kitchen object know which counter it is on
    * Let the counter know what is on it
    * set up a testing script to transfer kitchen object to second counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,13 +6,86 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
 
+    private KitchenObject kitchenObject;
+
+    /// <summary>
+    /// Update is called every frame
+    /// </summary>
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(kitchenObject);
+
+            if(kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+        
+    }
+
+    /// <summary>
+    /// Interact logic for the counter
+    /// Spawns a kitchen object if kitchen object is not present
+    /// </summary>
     public void Interact()
     {
-        Debug.Log("Interact");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO());
+    /// <summary>
+    /// Get the clear counter counter top point
+    /// </summary>
+    /// <returns>Counter Top Point</returns>
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    /// <summary>
+    /// Sets the kitchen object on the counter
+    /// </summary>
+    /// <param name="kitchenObject">Kitchen object to be set</param>
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    /// <summary>
+    /// Gets the kitchen object on the counter
+    /// </summary>
+    /// <returns>Kitchen object on the counter</returns>
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    /// <summary>
+    /// Removes kitchen object from counter
+    /// </summary>
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    /// <summary>
+    /// Check if counter is occupied by kitchen object
+    /// </summary>
+    /// <returns>True if kitchen object is present</returns>
+    public bool HasKitchenObject()
+    {
+        return (kitchenObject != null);
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,6 +6,8 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     /// <summary>
     /// Returns the KitchenObjectSO attached to the object
     /// </summary>
@@ -13,5 +15,38 @@ public class KitchenObject : MonoBehaviour
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    /// <summary>
+    /// Move object to new counter
+    /// </summary>
+    /// <param name="clearCounter">The new counter to move object to</param>
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter !=null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if(clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter is full");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    /// <summary>
+    /// Get the clear counter the object is on
+    /// </summary>
+    /// <returns>The counter the object is on</returns>
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
